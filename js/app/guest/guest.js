a64f27a01445696eb1c7646b18168f0591b8d054
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2025-05-27 10:56:20 +0700

    move guest name to bottom

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -74,7 +74,7 @@ export const guest = (() => {
             const div = document.createElement('div');
             div.classList.add('m-2');
 
-            const template = `<medium class="font-garamond mt-0 mb-1 mx-0 p-0">${'Dear, '+util.escapeHtml(guestName?.getAttribute('data-message'))}</small><p class="font-garamond m-0 p-0" style="font-size: 2rem">${util.escapeHtml(name)}</p><br><h2 class="font-garamond" style="font-size: 2rem;">We invite you to</h2>`;
+            const template = `<medium class="font-garamond mt-0 mb-1 mx-0 p-0">${'Dear, '+util.escapeHtml(guestName?.getAttribute('data-message'))}</small><p class="font-garamond m-0 p-0" style="font-size: 2rem">${util.escapeHtml(name)}</p>`;
             util.safeInnerHTML(div, template);
 
             guestName?.appendChild(div);
